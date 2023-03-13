cb1f437c2dcd9b9ea7d08e1bac0e3152ed731771
Antonet99 <antoniobaio90@gmail.com>
2023-03-13 01:41:14 +0100

    format

--- Chatbot_RASA/actions/actions.py
+++ Chatbot_RASA/actions/actions.py
@@ -11,7 +11,8 @@ from typing import Any, Text, Dict, List
 #
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-import requests, datetime
+import requests
+import datetime
 from weather import get_weather_data
 #
 #
@@ -28,6 +29,7 @@ from weather import get_weather_data
 #
 #         return []
 
+
 class ActionHelloWorld(Action):
 
     def name(self) -> Text:
@@ -40,106 +42,104 @@ class ActionHelloWorld(Action):
         dispatcher.utter_message(text="Hello World from my first action!")
 
         return []
-    
+
+
 class Weather(Action):
 
     def name(self) -> Text:
-            return "action_weather"
+        return "action_weather"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-            
-            city = tracker.get_slot('city')
-            wx_type = tracker.get_slot('wx_type')
-            
-            if wx_type is None:
-                wx_type = "meteo"
-                
-            #api_key = "74b04930d39039f068ed4796e9baf28a"
-            
-            # chiamata API che ritorna JSON con info meteo
-            weather_data = get_weather_data(city)
-            
-            # info in variabile current_weather
-            current_weather = weather_data['current']
-            
-            lat = weather_data['lat']
-            lon = weather_data['lon']
-            timezone = weather_data['timezone']
-            dt = str(datetime.datetime.fromtimestamp(current_weather['dt']))
-            temp = current_weather['temp']
-            percepita = current_weather['feels_like']
-            pressione = current_weather['pressure']
-            umidità = current_weather['humidity']
-            uvi = current_weather['uvi']
-            vento_velocità = current_weather['wind_speed']
-            vento_dir = current_weather['wind_deg']
-            descrizione_meteo = current_weather['weather'][0]['description']
-            if 'rain' in current_weather:
-                pioggia = current_weather['rain']['1h']
-            else:
-                pioggia = 0
-            #if 'alerts' in weather_data:
-                #sender = weather_data['alerts'][0]['sender_name']
-                #event = weather_data['alerts'][0]['event']
-                #alert_descript = weather_data['alerts'][0]['description']
-            #else:
-                #alerts = None
-                
-            response = f"Mi dispiace, qualcosa è andato storto. Riprova!"
-            
-            match wx_type.lower():
-                
-                # ritorna tutto il meteo
-                case "meteo":
-                    
-                    if pioggia > 0:
-                        response = f"Meteo attuale a {city} ({lat}, {lon}) il {dt}: {descrizione_meteo}\n" \
-                                    f"Temperatura: {temp} °C (Percepita {percepita} °C)\n" \
-                                    f"Umidità: {umidità} %\n" \
-                                    f"Pressione: {pressione} hPa\n" \
-                                    f"Indice UV: {uvi}\n" \
-                                    f"Vento: {vento_velocità} m/s, {vento_dir}°\n" \
-                                    f"Pioggia (ultim'ora): {pioggia} mm\n" \
-                                    f"Timezone: {timezone}"
-                    else:
-                        response = f"Meteo attuale a {city} ({lat}, {lon}) il {dt}: {descrizione_meteo}\n" \
-                                    f"Temperatura: {temp} °C (Percepita {percepita} °C)\n" \
-                                    f"Umidità: {umidità} %\n" \
-                                    f"Pressione: {pressione} hPa\n" \
-                                    f"Indice UV: {uvi}\n" \
-                                    f"Vento: {vento_velocità} m/s, {vento_dir}°\n" \
-                                    f"Attualmente non sta piovendo \n" \
-                                    f"Timezone: {timezone}"
-                                    #f"Alerts:\n" \
-                                    #f" - sender: {sender}\n" \
-                                    #f" - event: {event}\n" \
-                                    #f" - description: {alert_descript}\n" \
-                 
-                # ritorna il singolo parametro richiesto dall'utente
-                case "vento":
-                    response = f"La velocità attuale del vento a {city} ({lat}, {lon}) il {dt} è di {vento_velocità} metri al secondo da {vento_dir} gradi. "
-                
-                case "temperatura":
-                    response = f"La temperatura attuale a {city} ({lat}, {lon}) il {dt} è {temp}°C. "
-                
-                case "pressione":
-                    response = f"La pressione attuale dell'aria a {city} ({lat}, {lon}) il {dt} è {pressione} hPa. "
-                
-                case "umidità":
-                    response = f"L'umidità attuale a {city} ({lat}, {lon}) il {dt} è {umidità}%. "
-                
-                case "uvi":
-                    response = f"L'indice UV attuale a {city} ({lat}, {lon}) il {dt} è {uvi}. "
-                
-                case "pioggia":      
-                    if pioggia != 0:
-                        response = f"La pioggia nell'ultima ora a {city} ({lat}, {lon}) il {dt} è stata di {pioggia} mm. "
-                    else:
-                        response=f"Non sta piovendo in questo momento a {city} ({lat}, {lon}). "
 
-            dispatcher.utter_message(response)
-                
-            return []
+        city = tracker.get_slot('city')
+        wx_type = tracker.get_slot('wx_type')
 
+        if wx_type is None:
+            wx_type = "meteo"
+
+        # chiamata API che ritorna JSON con info meteo
+        weather_data = get_weather_data(city)
+
+        # info in variabile current_weather
+        current_weather = weather_data['current']
+
+        lat = weather_data['lat']
+        lon = weather_data['lon']
+        timezone = weather_data['timezone']
+        dt = str(datetime.datetime.fromtimestamp(current_weather['dt']))
+        temp = current_weather['temp']
+        percepita = current_weather['feels_like']
+        pressione = current_weather['pressure']
+        umidità = current_weather['humidity']
+        uvi = current_weather['uvi']
+        vento_velocità = current_weather['wind_speed']
+        vento_dir = current_weather['wind_deg']
+        descrizione_meteo = current_weather['weather'][0]['description']
+        if 'rain' in current_weather:
+            pioggia = current_weather['rain']['1h']
+        else:
+            pioggia = 0
+        # if 'alerts' in weather_data:
+            # sender = weather_data['alerts'][0]['sender_name']
+            # event = weather_data['alerts'][0]['event']
+            # alert_descript = weather_data['alerts'][0]['description']
+        # else:
+            # alerts = None
+
+        response = f"Mi dispiace, qualcosa è andato storto. Riprova!"
+
+        match wx_type.lower():
+
+            # ritorna tutto il meteo
+            case "meteo":
+
+                if pioggia > 0:
+                    response = f"Meteo attuale a {city} ({lat}, {lon}) il {dt}: {descrizione_meteo}\n" \
+                        f"Temperatura: {temp} °C (Percepita {percepita} °C)\n" \
+                        f"Umidità: {umidità} %\n" \
+                        f"Pressione: {pressione} hPa\n" \
+                        f"Indice UV: {uvi}\n" \
+                        f"Vento: {vento_velocità} m/s, {vento_dir}°\n" \
+                        f"Pioggia (ultim'ora): {pioggia} mm\n" \
+                        f"Timezone: {timezone}"
+                else:
+                    response = f"Meteo attuale a {city} ({lat}, {lon}) il {dt}: {descrizione_meteo}\n" \
+                        f"Temperatura: {temp} °C (Percepita {percepita} °C)\n" \
+                        f"Umidità: {umidità} %\n" \
+                        f"Pressione: {pressione} hPa\n" \
+                        f"Indice UV: {uvi}\n" \
+                        f"Vento: {vento_velocità} m/s, {vento_dir}°\n" \
+                        f"Attualmente non sta piovendo \n" \
+                        f"Timezone: {timezone}"
+                    # f"Alerts:\n" \
+                    # f" - sender: {sender}\n" \
+                    # f" - event: {event}\n" \
+                    # f" - description: {alert_descript}\n" \
+
+            # ritorna il singolo parametro richiesto dall'utente
+            case "vento":
+                response = f"La velocità attuale del vento a {city} ({lat}, {lon}) il {dt} è di {vento_velocità} metri al secondo da {vento_dir} gradi. "
+
+            case "temperatura":
+                response = f"La temperatura attuale a {city} ({lat}, {lon}) il {dt} è {temp}°C. "
+
+            case "pressione":
+                response = f"La pressione attuale dell'aria a {city} ({lat}, {lon}) il {dt} è {pressione} hPa. "
+
+            case "umidità":
+                response = f"L'umidità attuale a {city} ({lat}, {lon}) il {dt} è {umidità}%. "
+
+            case "uvi":
+                response = f"L'indice UV attuale a {city} ({lat}, {lon}) il {dt} è {uvi}. "
+
+            case "pioggia":
+                if pioggia != 0:
+                    response = f"La pioggia nell'ultima ora a {city} ({lat}, {lon}) il {dt} è stata di {pioggia} mm. "
+                else:
+                    response = f"Non sta piovendo in questo momento a {city} ({lat}, {lon}). "
+
+        dispatcher.utter_message(response)
+
+        return []
--- Chatbot_RASA/weather.py
+++ Chatbot_RASA/weather.py
@@ -3,8 +3,9 @@ import json
 import sys
 import platform
 
+
 def get_api():
-    
+
     os = platform.system()
 
     match os.lower():
@@ -14,48 +15,50 @@ def get_api():
             path = "/home/antonio/Scrivania/ProgettiDS/config.json"
         case "windows":
             path = "AGGIUNGI PATH"
-            
+
     with open(path) as f:
         data = json.load(f)
 
     owm_api = data['owm_api']
-    
+
     return owm_api
-    
+
+
 def get_lat_lon(city):
-    
+
     owm_api = get_api()
-    
-    url = ( 
+
+    url = (
         f"http://api.openweathermap.org/geo/1.0/direct?"
         f"q={city}&limit=2&appid={owm_api}"
     )
     response = requests.get(url)
-    
+
     if response.status_code == 200:
-         # parsing della risposta JSON
+        # parsing della risposta JSON
         data = response.json()
         if len(data) > 0:
             # accesso alle coordinate geografiche della città
             lat = data[0]['lat']
             lon = data[0]['lon']
-            
+
             lat = round(lat, 1)
             lon = round(lon, 1)
-            
+
             return lat, lon
         else:
-            print(f"Non sono state trovate informazioni geografiche per la città di {city}")
-    else: 
+            print(
+                f"Non sono state trovate informazioni geografiche per la città di {city}")
+    else:
         print("Errore durante la richiesta delle informazioni geografiche")
 
 
 def get_weather_data(city):
     """Get weather data from the API and return the necessary data."""
-    
+
     owm_api = get_api()
     lat, lon = get_lat_lon(city)
-    
+
     url = (
         f"https://api.openweathermap.org/data/3.0/onecall?"
         f"lat={lat}&lon={lon}"
@@ -64,11 +67,11 @@ def get_weather_data(city):
         f"&exclude=hourly,daily,minutely"
         f"&appid={owm_api}"
     )
-    
+
     try:
         response = requests.get(url)
         response.raise_for_status()
-        
+
     except requests.HTTPError:
         status = response.status_code
         if status == 401:
